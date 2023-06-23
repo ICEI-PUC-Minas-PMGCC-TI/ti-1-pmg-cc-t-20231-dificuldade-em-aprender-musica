75e558886989da3786be7675e5ac3581269ad806
Felipe Vilhena Dias <121449886+felipevidias@users.noreply.github.com>
2023-06-23 16:21:05 -0300

    Add files via upload

---
+++ codigo/PlataformaVideoAulas/videos.js
@@ -0,0 +1,30 @@
+export default [
+    {
+      src: "./img/01.mp4",
+      title: "Aula 1",
+    },
+    {
+      src: "./img/02.mp4",
+      title: "Aula 2",
+    },
+    {
+      src: "./img/03.mp4",
+      title: "Aula 3",
+    },
+    {
+      src: "./img/04.mp4",
+      title: "Aula 4",
+    },
+    {
+      src: "./img/05.mp4",
+      title: "Aula 5",
+    },
+    {
+      src: "./img/06.mp4",
+      title: "Aula 6",
+    },
+    {
+      src: "./img/07.mp4",
+      title: "Aula 7",
+    },
+  ];
